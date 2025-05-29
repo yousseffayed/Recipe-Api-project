c920be3bec6361d18ec37a4d70274e41b9ee0137
yousseffayed <yousseffayed79@gmail.com>
2025-05-29 16:28:50 +0300

    Please enter the commit message for your changes. Lines starting

---
+++ app/app/tests.py
@@ -0,0 +1,18 @@
+from django.test import SimpleTestCase
+from app import calc
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
+
+    def test_add_numbers(self):
+        """Test adding numbers together."""
+        res = calc.add(5, 6)
+
+        self.assertEqual(res, 11)
+
+
+    def test_sub_numbers(self):
+        """Test adding numbers together."""
+        res = calc.sub(10, 15)
+
+        self.assertEqual(res, 5)
+    
